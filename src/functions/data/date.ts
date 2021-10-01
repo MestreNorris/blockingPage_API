9c932b4afb0e2554c3b3f6f36c8d0986c4361b28
Alan Braun Pereira <alanbraunpereira@hotmail.com>
2021-10-01 16:11:47 -0300

    Enable cors

--- src/functions/data/date.ts
+++ src/functions/data/date.ts
@@ -1,12 +1,12 @@
 const dateNow = () => {
-    const dateNow = new Date().toLocaleString("pt-br", { timeZone: "America/Sao_Paulo" });
-    return (dateNow.split(' ')[0]);
+    const date = new Date().toLocaleString("pt-br", { timeZone: "America/Sao_Paulo" });
+    return (date.split(' ')[0]);
 }
 
 const formatDate = (verification_time) => {
     const date = verification_time.split('T')[0];
-    const formatDate = date.split('-');
-    return (formatDate[2] + "/" + formatDate[1] + "/" + formatDate[0]);
+    const format = date.split('-');
+    return (format[2] + "/" + format[1] + "/" + format[0]);
 }
 
 export { dateNow, formatDate }
